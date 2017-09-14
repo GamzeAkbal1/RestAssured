4336692482f7f79bcea7a628fd069e574a1b5e6d
GamzeAkbal1 <gakbal112@gmail.com>
2017-09-14 16:46:24 +0300

    Added Init Method and checkCount -json

--- src/test/java/apiAssertionTest/assertionTest.java
+++ src/test/java/apiAssertionTest/assertionTest.java
@@ -1,7 +1,45 @@
 package apiAssertionTest;
 
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.response.ValidatableResponse;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.HashMap;
+
+import static io.restassured.RestAssured.*;
+import static com.jayway.restassured.RestAssured.given;
+import static org.codehaus.groovy.tools.shell.util.Logger.io;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.equalToIgnoringCase;
+import static io.restassured.path.xml.XmlPath.*;
+import static org.hamcrest.Matchers.is;
+
 /**
  * Created by xernea on 14.09.2017.
  */
 public class assertionTest {
+
+    static HashMap<String,Object> params = new HashMap<String, Object>();
+
+    @BeforeClass
+    public static  void Init()
+    {
+        RestAssured.baseURI = "https://query.yahooapis.com";
+        RestAssured.basePath = "/v1/public";
+        params.put("q","SELECT * FROM yahoo.finance.xchange WHERE pair in (\"EURUSD\",\"GBPUSD\")");
+        params.put("format","json");
+        params.put("env","store://datatables.org/alltableswithkeys");
+
+    }
+
+    @Test
+    public void checkCount()
+    {
+         given().parameters(params)
+                .when().get("/yql")
+                .then().body("query.count",is(2));
+    }
 }

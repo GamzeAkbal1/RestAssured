329bf09880ac7cbe5f0668262a1dc2d1a71d6581
GamzeAkbal1 <gakbal112@gmail.com>
2017-09-19 11:39:37 +0300

    Added responseSpec class and checkCount Method

---
+++ src/test/java/responseSpec/responseSpecTest.java
@@ -0,0 +1,65 @@
+package responseSpec;
+
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.builder.ResponseSpecBuilder;
+import com.jayway.restassured.specification.RequestSpecification;
+//import io.restassured.builder.RequestSpecBuilder;
+import com.jayway.restassured.builder.RequestSpecBuilder;
+//import io.restassured.specification.RequestSpecification;
+import com.jayway.restassured.specification.ResponseSpecification;
+import org.apache.http.client.methods.RequestBuilder;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.*;
+import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+/**
+ * Created by xernea on 19.09.2017.
+ */
+public class responseSpecTest {
+
+    static RequestSpecBuilder reqBuilder;
+    static RequestSpecification reqSpec;
+    static ResponseSpecBuilder respBuilder;
+    static ResponseSpecification respSpec;
+
+    @BeforeClass
+    public static void Init(){
+
+        RestAssured.baseURI = "https://query.yahooapis.com";
+        RestAssured.basePath = "/v1/public";
+        reqBuilder = new RequestSpecBuilder();
+        respBuilder = new ResponseSpecBuilder();
+        reqBuilder.addParam("q","SELECT * FROM yahoo.finance.xchange WHERE pair in (\"EURUSD\",\"GBPUSD\")");
+        reqBuilder.addParam("env","store://datatables.org/alltableswithkeys");
+        reqBuilder.addParam("format","json");
+        reqSpec = reqBuilder.build();
+        respSpec = respBuilder.build();
+
+
+
+    }
+
+    @Test
+    public void checkCount(){
+
+        given().spec(reqSpec)
+                .when().get("/yql")
+                .then()
+                .spec(respSpec.expect().body("query.count",equalTo(2)))
+                .spec(respSpec.statusCode(200));
+
+                //spec(respSpec).log().body().statusCode(200);
+
+    }
+
+    @Test
+    public void checkHeaders(){
+
+
+    }
+}
